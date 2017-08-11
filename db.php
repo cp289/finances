d4dbf0541dc589d955c6cc57dbfd4eec40ecf863
cp289 <charles.parham@colby.edu>
2017-08-10 21:39:25 -0400

    support prepare multi query

--- db.php
+++ db.php
@@ -5,12 +5,54 @@
 
 class db extends mysqli {
 	
-	public function get_results($query) {
-		$res = $this->multi_query($query);
-		if ( ! $res ) {
-			print_r($this->error);
+	// params probably only works for single queries...
+	public function get_results($sql, $params=array()) {
+		$queries = explode(';',$sql);
+		
+		$rows = array();
+		foreach ( $queries as $query ) {
+			if ( $query == null ) continue;
+			//prepare statement
+			if ( ! ($stmt = $this->prepare($query)) ) {
+				db_error($this,"Prepare failed");
+			}
+			// Bind parameters
+			if ( sizeof($params) > 0 ) {
+				$dtypes = "";
+				foreach ( $params as $p ) {
+					switch( gettype ( $p ) ) {
+						case 'string':
+							$dtypes .= 's';
+							break;
+						case 'integer':
+							$dtypes .= 'i';
+							break;
+						case 'double':
+							$dtypes .= 'd';
+							break;
+						default:
+							$dtypes .= 'b';
+					}
+				}
+				if ( ! $stmt->bind_param($dtypes, ...$params) ) {
+					db_error($stmt,"Bind failed");
+				}
+			}
+			// Execute query and get result set
+			if ( ! $stmt->execute() ) {
+				db_error($stmt,"Execute failed");
+			} else {
+				do {
+					if ( $res = $stmt->get_result() ) {
+						while ( $row = $res->fetch_object() ) {
+							$rows[] = $row;
+						}
+						$res->close();
+					}
+				} while ( $stmt->more_results() && $stmt->next_result() );
+			}
 		}
-		return gettype($res)!="boolean" ? $res->fetch_all() : false;
+		return $rows;
 	}
 }
 
