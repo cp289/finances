6967f8559a36e6aaad16a2fc6c6b260ecb8e678d
cp289 <charles.parham@colby.edu>
2017-08-10 21:40:14 -0400

    add preformatted print, db error print

--- functions.php
+++ functions.php
@@ -1,9 +1,21 @@
 <?php
 
 // for debugging...
-function pp($var, $die=true) {
+function pp($var, $die=false) {
 	print('<pre>');
 	print_r($var);
 	print('</pre>');
 	if ($die) die();
+}
+
+function pre($var) {
+	print('<pre>');
+	print($var);
+	print('</pre>');
+}
+
+function db_error($obj, $message) {
+	$out = "<strong>Error: <em>{$message}</em> ({$obj->errno})</strong> {$obj->error}";
+	pre($out);
+	return $out;
 }
