50dd48f0bafb48f1e222b228a46d4d3b434bb9ca
cp289 <charles.parham@colby.edu>
2017-08-10 21:43:50 -0400

    improve _createDatabase, make addAccount & addTrans

--- finance_api.php
+++ finance_api.php
@@ -4,39 +4,37 @@ class finance_api {
 	
 	private $dbname;
 	
-	function __construct ( $dbname ) {
+	public function __construct ( $dbname='finances' ) {
+		$this->dbname = $dbname;
+		$this->_createDatabase();
+		
 		global $db;
-		$this->createFinancesDatabase($dbname);
 		$db->select_db($dbname);
 	}
 	
-	public function createFinancesDatabase($dbname){
+	private function _createDatabase(){
 		global $db;
 		
 		// create finances and select it
-		$db->query("CREATE DATABASE IF NOT EXISTS `$dbname`;");
+		$db->get_results("CREATE DATABASE IF NOT EXISTS `{$this->dbname}`;");
 		
 		// create data tables
 		$sql = "CREATE TABLE IF NOT EXISTS transactions (
-			`id` INT NOT NULL,
-			`time_ent` TIMESTAMP NOT NULL,
+			`id` mediumint NOT NULL AUTO_INCREMENT,
+			`time_ent` datetime NOT NULL,
 			`date` DATE NOT NULL,
-			`year` SMALLINT NOT NULL,
-			`month` SMALLINT NOT NULL,
-			`day` SMALLINT NOT NULL,
 			`location` VARCHAR(50) NOT NULL,
-			`mon_orig` smallint NOT NULL,
-			`mon_dest` smallint NOT NULL,
-			`type` VARCHAR(50) NOT NULL,
+			`origin` smallint NOT NULL,
+			`destin` smallint NOT NULL,
 			`amount` DECIMAL(10,2) NOT NULL,
-			`categ` VARCHAR(50) NOT NULL,
-			`comments` VARCHAR(100) NOT NULL,
+			`descr` VARCHAR(150),
 			PRIMARY KEY (id)
 		);";
 		$sql .= "CREATE TABLE IF NOT EXISTS accounts (
 			`id` mediumint NOT NULL AUTO_INCREMENT,
 			`type` smallint NOT NULL,
 			`name` varchar(50) NOT NULL,
+			`descr` varchar(150),
 			PRIMARY KEY (id)
 		);";
 		$sql .= "CREATE TABLE IF NOT EXISTS events (
@@ -44,20 +42,47 @@ class finance_api {
 			`name` varchar(50) NOT NULL,
 			`date` date NOT NULL,
 			`recur` smallint NOT NULL,
-			`descr` varchar(150) NOT NULL,
+			`descr` varchar(150),
 			PRIMARY KEY (id)
 		);";
+		$sql .= "CREATE TABLE IF NOT EXISTS tags (
+			`id` mediumint NOT NULL AUTO_INCREMENT,
+			`name` varchar(50) NOT NULL,
+			`descr` varchar(150),
+			PRIMARY KEY (id)
+		);";
+		$sql .= "CREATE TABLE IF NOT EXISTS tag_map (
+			`id` mediumint NOT NULL AUTO_INCREMENT,
+			`trans` mediumint NOT NULL,
+			`tag` mediumint NOT NULL,
+			PRIMARY KEY (id)
+		);";
+		//$sql .= "CREATE TABLE IF NOT EXISTS tag_map ("
 		$db->get_results($sql);
 	}
-
-	public function addAccount($name, $type) {
+	
+	private function _resetDatabase() {
 		global $db;
-		$out=$db->query("INSERT INTO accounts (type,name) VALUES ('$type','$name');");
-		$sql = "SELECT * FROM accounts;";
-		$out=$db->get_results($sql);
+		$db->get_results( "DROP DATABASE $dbname" );
+		$this->_createDatabase();
+	}
+	
+	// Important: escape all parameters (find right method)
+	public function addAccount($name, $type, $descr='NULL') {
+		global $db;
+		$db->get_results("INSERT INTO accounts (type,name,descr) VALUES ('$type','$name','$descr');");
+	}
+	
+	public function addTrans($date, $location, $origin, $destin, $amount, $descr) {
+		global $db;
+		$time_ent = date('Y-m-d H:i:s');
+		$params = array( $time_ent,$date,$location,$origin,$destin,$amount,$descr );
+		$sql = "INSERT INTO transactions (time_ent,date,location,origin,destin,amount,descr)
+			VALUES (?,?,?,?,?,?,?);";
+		$db->get_results($sql, $params);
 	}
 }
 
 // create global API object
-global $fin_api;
-$fin_api = new finance_api( 'finances' );
+global $finApi;
+$finApi = new finance_api();
