767cd4229034b8d762b7dd1628dc9ad7336032ab
cp289 <cparham289@gmail.com>
2017-08-30 02:12:47 -0400

    update constants

--- const.php
+++ const.php
@@ -2,9 +2,9 @@
 
 // database
 const PRINT_DB_ERRORS = true;
-const DB_NAME = 'finances';
+const DB_NAME = 'ParhamFinances';
 const MYSQL_USER = 'root';
-const MYSQL_PASS = '';
+const MYSQL_PASS = 'rootpass69';
 
 // server
 const SERVER_DOMAIN = 'localhost';
